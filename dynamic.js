2e61fed0acfbe6de925e322384dc6dbe3e5c1e19
ICA109 <t.yin1@outlook.com>
2017-12-22 16:37:48 -0800

    Add in changing background function

---
+++ dynamic.js
@@ -0,0 +1,2 @@
+var bgimage = "http://interfacelift.com/wallpaper/Db3c61e5/03514_duskatlakezurich_1920x1080.jpg"
+document.getElementById("body#gsr.hp.vasq").style.backgroundImage = "url(" + bgimage + ")";
